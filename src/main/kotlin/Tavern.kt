2bc103e40d0d5ddcc761be8c1a4af16807eeee64
PHM1605 <PHM1605@gmail.com>
2024-04-01 00:42:03 +0700

    updates

---
+++ src/main/kotlin/Tavern.kt
@@ -0,0 +1,8 @@
+private const val TAVERN_MASTER = "Taernyl"
+private const val TAVERN_NAME = "$TAVERN_MASTER's Folly"
+
+fun visitTavern() {
+    narrate("$heroName enters $TAVERN_NAME")
+    val patrons = listOf("Eli", "Mordoc", "Sophie")
+    println(patrons.get(0))
+}
