82ffbdf71856cec38beb5814c54ff5aa512cc0b9
PHM1605 <PHM1605@gmail.com>
2024-04-15 00:29:24 +0700

    updates

--- src/main/kotlin/com/bignerdranch/nyethack/NyetHack.kt
+++ src/main/kotlin/com/bignerdranch/nyethack/NyetHack.kt
@@ -6,7 +6,7 @@ fun main() {
     narrate("Welcome to Nyethack!")
     val playerName = promptHeroName()
     player = Player(playerName)
-    //changeNarratorMood()
+    changeNarratorMood()
 
     Game.play()
 }
@@ -15,13 +15,11 @@ private fun makeYellow(message:String) = "\u001b[33;1m$message\u001b[0m"
 
 private fun promptHeroName(): String {
     narrate("A hero enters the town of Kronstadt. What is their name?", ::makeYellow)
-    /*
+
     val input = readLine()
     require(input != null && input.isNotEmpty()) {
         "The hero must have a name."
     }
     return input
-    */
-    println("Madrigal")
-    return "Madrigal"
+
 }
